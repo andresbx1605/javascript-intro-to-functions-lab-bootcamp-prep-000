1a5a7b5e034c5147655e7c034fdd8b349886a5dc
Andres Mercado <andresmercado69@gmail.com>
2019-11-11 13:07:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ function sayHiToGrandma(string){
     return "I can't hear you!" //if it is lowercase, returns this
   } else if(string === "I love you, Grandma."){ //if string isn't lowercase, checks to see if string is 'I love you, Grandma.'
     return "I love you, too." //if it is, returns this
-    if(string === "I love you, too");
-    return "YES INDEED!"
-    if(string.toUpperCase() === string);
+    
   }
 }
 
-  
+function sayHiToGrandma(string){
+  if(string.toUpperCase() === string)
+  return "YES INDEED!"
+}
   
 
 
