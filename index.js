409d99bc80778dfc6ce98ed1d645e57cf5f028ed
Andres Mercado <andresmercado69@gmail.com>
2019-11-12 00:33:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,14 +21,6 @@ function sayHiToGrandma(string){
 }
 
 
-function sayHiToGrandma(string){
-  if(string === "I love you, too."){
-    return "YES INDEED!"
-  }
-}
-
-  
-
 
 function whisper(string) {
   return string.toLowerCase()
